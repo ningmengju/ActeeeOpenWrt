3a36ee02e911d05971f4dbf0e6bcf7ff42a2cac3
ningmengju <134126831+ningmengju@users.noreply.github.com>
2024-11-06 12:59:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,7 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sudo apt-get install ocaml-nox
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 make download -j8 V=s
